07ca912134bca3816911ac66622839d2a1039d9e
hafizhme <hafizhharsono@gmail.com>
2015-11-29 11:08:48 +0700

    cek tak

--- author.h
+++ author.h
@@ -22,42 +22,7 @@ struct elmAuthor
 struct Author
 {
 	adrAuthor first;
-};#ifndef AUTHOR_H
-#define AUTHOR_H
-#include "category.h"
-#include <string>
-
-using namespace std;
-
-struct infoAuthor
-{
-	string idAuthor;
-	string nameAuthor;
 };
-
-typedef struct elmAuthor *adrAuthor;
-
-struct elmAuthor
-{
-	infoAuthor info;
-	adrAuthor prev, next;
-	Category category;
-};
-struct Author
-{
-	adrAuthor first;
-};
-
-void createAuthor(Author *A);
-adrAuthor alocate(infoAuthor iA);
-void insertAuthor(Author *A, infoAuthor iA);
-void inputAuthor(Author *A, int sumAuthor);
-adrAuthor findAuthor(Author A, infoAuthor F);
-void deleteAuthor(Author *A, infoAuthor F);
-void printAuthor(Author A);
-
-#endif
-
 
 void createAuthor(Author *A);
 adrAuthor alocate(infoAuthor iA);
